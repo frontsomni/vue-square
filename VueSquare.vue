dd2012a8fb198cb8401f0924eff60a0a17871895
liuyue <18519127046@163.com>
2020-12-10 11:17:24 +0800

    Release 2.3.0

---
+++ VueSquare.vue
@@ -0,0 +1,29 @@
+<template>
+  <div><span :style="{
+    color: textColor
+  }">{{text}}</span></div>
+</template>
+
+<script>
+export default {
+  name: 'VueSquare',
+  props: {
+    text: {
+      type: String,
+      default: 'vue-square'
+    },
+    textColor: {
+      type: String,
+      default: '#fff'
+    }
+  }
+}
+</script>
+
+<style scoped>
+div{
+  width: 100px;
+  height: 100px;
+  background: #c20;
+}
+</style>
